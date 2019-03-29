408af2823055008ee913474e79b11ce09ed54734
kahheng93 <kahheng93@gmail.com>
2019-03-29 10:15:20 +0000

    Change all request type to put

--- routes/index.js
+++ routes/index.js
@@ -7,12 +7,12 @@ const router = express.Router();
 //For cross domain api request
 router.all('/api/v1/mobileapi/createNotification', HelperController.handleCorsPreflight);
 
-router.get('/api/v1/auth/login', MobileApiController.login);
-router.get('/api/v1/mobileapi/getProfile', HelperController.authenticated, MobileApiController.getProfile);
-router.get('/api/v1/mobileapi/getTimetable', HelperController.authenticated, MobileApiController.getTimetable);
-router.get('/api/v1/mobileapi/getLesson', HelperController.authenticated, MobileApiController.getLesson);
-router.get('/api/v1/mobileapi/getSubjectAttendance', HelperController.authenticated, MobileApiController.getSubjectAttendance);
-router.get('/api/v1/mobileapi/getNotifiationList', HelperController.authenticated, MobileApiController.getNotifiationList);
+router.put('/api/v1/auth/login', MobileApiController.login);
+router.put('/api/v1/mobileapi/getProfile', HelperController.authenticated, MobileApiController.getProfile);
+router.put('/api/v1/mobileapi/getTimetable', HelperController.authenticated, MobileApiController.getTimetable);
+router.put('/api/v1/mobileapi/getLesson', HelperController.authenticated, MobileApiController.getLesson);
+router.put('/api/v1/mobileapi/getSubjectAttendance', HelperController.authenticated, MobileApiController.getSubjectAttendance);
+router.put('/api/v1/mobileapi/getNotifiationList', HelperController.authenticated, MobileApiController.getNotifiationList);
 
 router.put('/api/v1/mobileapi/updateProfile', HelperController.authenticated, MobileApiController.updateProfile);
 router.put('/api/v1/mobileapi/generateLessonAttendanceQrCode', HelperController.authenticated, MobileApiController.generateLessonAttendanceQrCode);
